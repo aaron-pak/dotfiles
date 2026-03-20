6b49f9ceeeabd452c045fc201e6b854e4795afc6
Aaron Pak <aaron@aaronpak.dev>
2026-03-20 16:29:19 -0400

    removed old skills

---
+++ home/.codex/shell_snapshots/019d0826-94f4-72c0-991b-08541049cb71.1773958240012111000.sh
@@ -0,0 +1,40 @@
+# Snapshot file
+# Unset all aliases to avoid conflicts with functions
+unalias -a 2>/dev/null || true
+# Functions
+
+# setopts 2
+setopt nohashdirs
+setopt login
+
+# aliases 2
+alias run-help=man
+alias which-command=whence
+
+# exports 26
+export BUN_INSTALL=/Users/aaronpak/.bun
+export CODEX_MANAGED_BY_BUN=1
+export COLORTERM=truecolor
+export COMMAND_MODE=unix2003
+export GHOSTTY_BIN_DIR=/Applications/Ghostty.app/Contents/MacOS
+export GHOSTTY_RESOURCES_DIR=/Applications/Ghostty.app/Contents/Resources/ghostty
+export GHOSTTY_SHELL_FEATURES=cursor:blink,path,title
+export HOME=/Users/aaronpak
+export LANG=en_US.UTF-8
+export LOGNAME=aaronpak
+export LaunchInstanceID=04EFC12A-4643-4F70-A616-ADD28577B2AC
+export OSLogRateLimit=64
+export SECURITYSESSIONID=186ae
+export SHELL=/bin/zsh
+export SSH_AUTH_SOCK=/private/tmp/com.apple.launchd.arxWIgOxY2/Listeners
+export TERM=xterm-ghostty
+export TERMINFO=/Applications/Ghostty.app/Contents/Resources/terminfo
+export TERM_PROGRAM=ghostty
+export TERM_PROGRAM_VERSION=1.3.1
+export TMPDIR=/var/folders/8f/q6szh8md5tzdls6csrkc59m00000gn/T/
+export USER=aaronpak
+export XDG_DATA_DIRS=/usr/local/share:/usr/share:/Applications/Ghostty.app/Contents/Resources/ghostty/..
+export XPC_FLAGS=0x0
+export XPC_SERVICE_NAME=0
+export __CFBundleIdentifier=com.mitchellh.ghostty
+export __CF_USER_TEXT_ENCODING=0x1F5:0x0:0x0
